e509c601c1a447bb54d058b7c5903bfbf5b269e0
Philip Antin <philip.antin@gmail.com>
2026-04-12 19:37:36 -0500

    chore: improve site history

--- libs/resume/past/src/lib/past.tsx
+++ libs/resume/past/src/lib/past.tsx
@@ -33,15 +33,41 @@ export const Past: React.FC = () => {
                     boxShadow: '0 4px 20px rgba(0,0,0,0.05)'
                 }}>
                     <Typography variant="h4" component="div" gutterBottom sx={{ fontSize: '180%', textTransform: 'uppercase', mb: 2 }}>
-                        "How it's going" vs. "How it started"
+                        "How it started"
                     </Typography>
                     <Box sx={{ width: '100px', height: '2px', bgcolor: 'primary.main', mx: 'auto', mb: 4 }} />
                     <Typography variant="body1" component="div" sx={{ fontSize: '110%', lineHeight: 1.45, mb: 4 }}>
-                        This website started in 2018, when Philip was first transitioning his career from education to software engineering. 
+                        This website started in 2018, when Philip was first transitioning his career from education to software engineering. He hand-coded it using PHP, adapting a template from an HTML and CSS course that he took to gain basic skills.
                     </Typography>
+                </Box>
+                <Box sx={{ 
+                    textAlign: 'center', 
+                    mb: 6, 
+                    width: { xs: '100%', md: '70%' }, 
+                    mx: 'auto',
+                    bgcolor: '#e8e8e8',
+                    p: { xs: 4, md: 8 },
+                    borderRadius: 2,
+                    boxShadow: '0 4px 20px rgba(0,0,0,0.05)'
+                }}>
+                    <Typography variant="h4" component="div" gutterBottom sx={{ fontSize: '180%', textTransform: 'uppercase', mb: 2 }}>
+                        "How it's going"
+                    </Typography>
+                    <Box sx={{ width: '100px', height: '2px', bgcolor: 'primary.main', mx: 'auto', mb: 4 }} />
                     <Typography variant="body1" component="div" sx={{ fontSize: '110%', lineHeight: 1.45, mb: 4 }}>
-                        It has been heavily revised in 2026: it now uses an Angular shell and header as a microfrontend, serving React components for most of the content. State passes between the two frameworks. This monorepo codebase is managed using Nx.
+                        Philip heavily revised the site in 2026.<br />It now uses an Angular shell and header as a microfrontend, serving React components for most of the content. State passes between the two frameworks. The monorepo codebase is managed using Nx.
                     </Typography>
+                </Box>
+                <Box sx={{ 
+                    textAlign: 'center', 
+                    mb: 6, 
+                    width: { xs: '100%', md: '70%' }, 
+                    mx: 'auto',
+                    bgcolor: '#e8e8e8',
+                    p: { xs: 4, md: 8 },
+                    borderRadius: 2,
+                    boxShadow: '0 4px 20px rgba(0,0,0,0.05)'
+                }}>
                     <Typography variant="body1" component="div" sx={{ fontSize: '110%', lineHeight: 1.45, mb: 4 }}>
                         You can see the original <em>hand-coded, PHP version</em> using the link below. It's a fun way to compare how his technical experience has evolved.
                     </Typography>
